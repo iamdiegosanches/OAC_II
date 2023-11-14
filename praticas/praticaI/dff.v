6f71772e9eefaa49b0910196a46e1bd91f9bf925
Diego <74864968+iamdiegosanches@users.noreply.github.com>
2023-11-14 19:19:33 -0300

    Update dff.v

--- praticas/praticaI/dff.v
+++ praticas/praticaI/dff.v
@@ -1,7 +1,6 @@
 module dff (
-    input d, rstb, clk;
-    output reg q
-);
+    input d, rstb, clk,
+    output reg q);
 
 always @(posedge clk) begin
     if (!rstb)
@@ -15,40 +14,33 @@ endmodule
 
 module tb_;
 
-reg clk;
-reg rst_n;
-reg d;
+    reg clk;
+    reg rstb;
+    reg d;
 
-wire q;
+    wire q;
 
-dff uut (
-    .d(d),
-    .rstb(~rst_n),
-    .clk(clk),
-    .q(q)
-);
+    dff uut (
+        .d(d),
+        .rstb(rstb),
+        .clk(clk),
+        .q(q)
+    );
 
-initial begin
-    $dumpfile("tb_.vcd");
-    $dumpvars(0, tb_);
+    always #2 clk = ~clk;
 
-    // Clock generation
-    always #(CLK_PERIOD/2) clk = ~clk;
+    initial begin
+        $monitor ("d=%b, rstb=%b, clk=%b, q=%b", d, rstb, clk, q);
 
-    // Testbench
-    #1 rst_n = 1'b0; clk = 1'b0;
-    #(CLK_PERIOD*3) rst_n = 1'b1;
-    #(CLK_PERIOD*3) rst_n = 1'b0; clk = 1'b0;
-    repeat(5) @(posedge clk);
-    rst_n = 1'b1;
-    @(posedge clk);
-    repeat(2) @(posedge clk);
-    // Assign values to d
-    d = 1'b0; // or any other value
-    @(posedge clk);
-    // Continue testing as needed
+        {clk, rstb, d} = 0;
+        #2 rstb <= 1;
+        #25;
+        d <= 1;
+        #25;
+        rstb <= 0;
+        #10;
 
-    $finish;
-end
+        #50 $finish;
+    end
 
 endmodule
